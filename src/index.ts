cdaafe8684be54794fa06af513342855d99cba25
Francisco Javier Alvarado <93677291+XxGhoulPr0xX@users.noreply.github.com>
2023-10-13 21:46:57 -0600

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Canister, query, text, update, Void } from 'azle';
 
-interface MultiplicationItem {
+interface ArithmeticItem {
     question: string;
     answer: number;
 }
 
-// This is a global variable that is stored on the heap
-let multiplicationTable: MultiplicationItem[] = [];
+let multiplicationTable: ArithmeticItem[] = [];
+let additionSubtractionTable: ArithmeticItem[] = [];
 let currentQuestion: string = '';
 
 function generateMultiplicationTable(number: number): void {
@@ -15,22 +15,50 @@ function generateMultiplicationTable(number: number): void {
     }
 }
 
+function generateAdditionSubtractionTable(number: number): void {
+    for (let i = 1; i <= 10; i++) {
+        additionSubtractionTable.push({ question: `${number} + ${i}`, answer: number + i });
+        additionSubtractionTable.push({ question: `${number} - ${i}`, answer: number - i });
+    }
+}
+
 export default Canister({
     // Query calls complete quickly because they do not go through consensus
-    getQuestion: query([], text, () => {
-        const randomIndex = Math.floor(Math.random() * multiplicationTable.length);
-        currentQuestion = multiplicationTable[randomIndex].question;
+    getQuestion: query([text], text, (type: string) => {
+        let table;
+        if (type === 'multiplication') {
+            table = multiplicationTable;
+        } else if (type === 'additionSubtraction') {
+            table = additionSubtractionTable;
+        } else {
+            throw new Error('Invalid type');
+        }
+        const randomIndex = Math.floor(Math.random() * table.length);
+        currentQuestion = table[randomIndex].question;
         return currentQuestion;
     }),
     // Update calls take a few seconds to complete
     // This is because they persist state changes and go through consensus
-    setNumber: update([text], Void, (numberText: string) => {
+    setNumber: update([text, text], Void, (type: string, numberText: string) => {
         const number = parseInt(numberText);
-        generateMultiplicationTable(number);
+        if (type === 'multiplication') {
+            generateMultiplicationTable(number);
+        } else if (type === 'additionSubtraction') {
+            generateAdditionSubtractionTable(number);
+        } else {
+            throw new Error('Invalid type');
+        }
     }),
-    checkAnswer: update([text], text, (userAnswerText: string) => {
+    checkAnswer: update([text, text], text, (question: string, userAnswerText: string) => {
         const userAnswer = parseInt(userAnswerText);
-        const correctAnswer = multiplicationTable.find(item => item.question === currentQuestion)?.answer;
+        let correctAnswer;
+        if (multiplicationTable.find(item => item.question === question)) {
+            correctAnswer = multiplicationTable.find(item => item.question === question)?.answer;
+        } else if (additionSubtractionTable.find(item => item.question === question)) {
+            correctAnswer = additionSubtractionTable.find(item => item.question === question)?.answer;
+        } else {
+            throw new Error('Invalid question');
+        }
         if (userAnswer === correctAnswer) {
             return 'Correcto';
         } else {
